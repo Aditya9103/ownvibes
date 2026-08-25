d5da7acf0a92eb1521525daf8ad7aadd6bde7aac
Aditya9103 <adityakumarsingh84260@gmail.com>
2026-08-25 15:53:07 +0530

    chore: rename herovideo.mp4 to hero.mp4 and update reference in Hero component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
           poster="/tishirtcombo.png"
           className="absolute inset-0 w-full h-full z-0 object-cover object-center"
         >
-          <source src="/herovideo.mp4" type="video/mp4" />
+          <source src="/hero.mp4" type="video/mp4" />
         </video>
 
         {/* Main Content Container */}
